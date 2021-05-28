8e2a0f66bf8e7bb3bd2755e677a1fdbf258a69b9
zhangguangying <786781353@qq.com>
2021-05-28 19:32:28 +0800

    404 状态码

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
 			"<a href=\"mailto:hyuiing@163.com\">hyuiing@163.com</a>")
 	default:
+		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
 			"<p>如有疑惑，请联系。</p>")
 	}
