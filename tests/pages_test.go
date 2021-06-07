db10a6302c86c1b416102c0e67cc592e80fd6d65
zhangguangying <786781353@qq.com>
2021-06-07 19:33:40 +0800

    第一个测试

---
+++ tests/pages_test.go
@@ -0,0 +1,20 @@
+package tests
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestHomePage(t *testing.T) {
+	baseUrl := "http://localhost:3000"
+
+	var (
+		resp *http.Response
+		err  error
+	)
+	resp, err = http.Get(baseUrl + "/")
+
+	assert.NoError(t, err, "有错误发生，err 不为空")
+	assert.Equal(t, 200, resp.StatusCode, "应返回状态码 200")
+}
