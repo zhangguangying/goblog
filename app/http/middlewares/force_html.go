9e31607365a60c68fdbdd6e5184614dd37e3f27d
zhangguangying <786781353@qq.com>
2021-06-08 16:51:35 +0800

    重构中间件

---
+++ app/http/middlewares/force_html.go
@@ -0,0 +1,22 @@
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+)
+
+func ForceHTML(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		h.ServeHTTP(w, r)
+	})
+}
+
+func RemoveTrailingSlash(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		}
+		next.ServeHTTP(w, r)
+	})
+}
--- main.go
+++ main.go
@@ -1,78 +1,14 @@
 package main
 
 import (
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
+	"github.com/zhangguangying/goblog/app/http/middlewares"
 	"github.com/zhangguangying/goblog/bootstrap"
-	"github.com/zhangguangying/goblog/pkg/database"
 	"net/http"
-	"net/url"
-	"strconv"
-	"strings"
 )
 
-var router *mux.Router
-var db *sql.DB
-
-type Article struct {
-	Title, Body string
-	ID          int64
-}
-
-func (a Article) Delete() (rowsAffect int64, err error) {
-	rs, err := db.Exec("delete from articles where id = " + strconv.FormatInt(a.ID, 10))
-	if err != nil {
-		return 0, err
-	}
-	if n, _ := rs.RowsAffected(); n > 0 {
-		return n, nil
-	}
-	return 0, nil
-}
-
-type ArticlesStoreData struct {
-	Title, Body string
-	URL         *url.URL
-	Errors      map[string]string
-}
-
-func forceHTMLMiddleware(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		h.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-func getArticleById(id string) (Article, error) {
-	var article Article
-	query := "select * from articles where id = ?"
-	err := db.QueryRow(query, id).Scan(&article.ID, &article.Title, &article.Body)
-	return article, err
-}
-
 func main() {
-	database.Initialize()
-	db = database.DB
-
 	bootstrap.SetDB()
-	router = bootstrap.SetupRoute()
+	router := bootstrap.SetupRoute()
 
-	router.Use(forceHTMLMiddleware)
-
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
-}
-
-func getRouteVariable(parameterName string, r *http.Request) string {
-	vars := mux.Vars(r)
-	return vars[parameterName]
+	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 }
