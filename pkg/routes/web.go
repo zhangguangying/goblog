48705595219b595021234b93319b7157cadd4547
zhangguangying <786781353@qq.com>
2021-06-09 18:28:17 +0800

    退出登录

--- pkg/routes/web.go
+++ pkg/routes/web.go
@@ -28,6 +28,7 @@ func RegisterWebRoutes(r *mux.Router) {
 	r.HandleFunc("/auth/do-register", auc.DoRegister).Methods("POST").Name("auth.doregister")
 	r.HandleFunc("/auth/login", auc.Login).Methods("GET").Name("auth.login")
 	r.HandleFunc("/auth/dologin", auc.DoLogin).Methods("POST").Name("auth.dologin")
+	r.HandleFunc("/auth/logout", auc.Logout).Methods("POST").Name("auth.logout")
 	//r.Use(middlewares.ForceHTML)
 
 	r.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
