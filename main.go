c2d9b080b815306dddd183e5e254dcc166cd2146
zhangguangying <786781353@qq.com>
2023-02-06 16:55:35 +0800

    添加关于页面

--- main.go
+++ main.go
@@ -6,7 +6,16 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	case "/about":
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+	default:
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+			"<p>如有疑惑，请联系我们。</p>")
+	}
 }
 
 func main() {
