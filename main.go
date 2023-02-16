bd800b00b3ebd8304468ae15a65fed1f85c99dd1
zhangguangying <786781353@qq.com>
2023-02-16 22:24:10 +0800

    重构 initDB 和 createTables

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"goblog/pkg/database"
 	"goblog/pkg/logger"
 	"goblog/pkg/route"
 	"goblog/pkg/types"
@@ -11,12 +12,9 @@ import (
 	"strconv"
 	"strings"
 	"text/template"
-	"time"
 	"unicode/utf8"
 
 	"github.com/gorilla/mux"
-
-	"github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
@@ -348,43 +346,9 @@ func articlesDeleteHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func initDB() {
-	var err error
-
-	config := mysql.Config{
-		User:                 "root",
-		Passwd:               "123456",
-		Addr:                 "localhost:3306",
-		Net:                  "tcp",
-		DBName:               "goblog",
-		AllowNativePasswords: true,
-	}
-	db, err = sql.Open("mysql", config.FormatDSN())
-	logger.LogError(err)
-
-	db.SetMaxOpenConns(25)
-	db.SetMaxIdleConns(25)
-	db.SetConnMaxLifetime(5 * time.Minute)
-
-	err = db.Ping()
-	logger.LogError(err)
-}
-
-func createTables() {
-	createArticlesSQL := `
-		CREATE TABLE IF NOT EXISTS articles(
-			id bigint(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-			title varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-			body longtext COLLATE utf8mb4_unicode_ci
-		)
-	`
-	_, err := db.Exec(createArticlesSQL)
-	logger.LogError(err)
-}
-
 func main() {
-	initDB()
-	createTables()
+	database.Initialize()
+	db = database.DB
 
 	route.Initialize()
 	router = route.Router
